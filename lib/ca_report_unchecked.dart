fb5c17450f0cf6de570be6f54af1acbc2a7ca771
Bingo3241 <bingo3241@hotmail.com>
2019-07-18 14:20:58 +0700

    link to ca-asset-detail

--- lib/ca_report_unchecked.dart
+++ lib/ca_report_unchecked.dart
@@ -42,6 +42,8 @@ class UncheckedReportState extends State<UncheckedReport> {
           child: Column(
             children: <Widget>[
               ListTile(
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/ca-asset-detail'),
                 leading: Container(
                   child: Image.network(
                       'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png'),
