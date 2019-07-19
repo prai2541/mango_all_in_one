28fd9dc66310e867176e9d28aff3323b1f4717ff
Bingo3241 <bingo3241@hotmail.com>
2019-07-19 16:32:32 +0700

    Add floatingactionbutton for scanning barcode

--- lib/ca_asset_detail.dart
+++ lib/ca_asset_detail.dart
@@ -1,20 +1,42 @@
 import 'dart:io';
-
-import 'package:app_ui/ca_asset_list.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
 
-import 'ca_unit_list.dart';
-import 'materialList.dart';
-
-class AssetDetail extends StatelessWidget{
+class AssetDetail extends StatelessWidget {
+  Future scan(BuildContext context) async {
+    try {
+      // String barcode = await BarcodeScanner.scan();
+      return Navigator.of(context).pushNamed('/ca-asset-detail');
+    } on PlatformException catch (e) {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text(e.code),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Asset Detail', style: TextStyle(fontFamily: 'Prompt', color: Colors.white)),
+        title: Text('Asset Detail',
+            style: TextStyle(fontFamily: 'Prompt', color: Colors.white)),
         iconTheme: IconThemeData(color: Colors.white),
         actions: <Widget>[
           IconButton(
@@ -23,12 +45,35 @@ class AssetDetail extends StatelessWidget{
               size: 30.0,
             ),
             onPressed: () {
-              Navigator.of(context).pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/home', (Route<dynamic> route) => false);
             },
           )
         ],
       ),
       body: AssetDetails(),
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(
+            bottom: ScreenUtil.instance.setHeight(20),
+            right: ScreenUtil.instance.setWidth(10)),
+        child: Container(
+          // height: ScreenUtil.instance.setHeight(60),
+          // width: ScreenUtil.instance.setWidth(60),
+          child: FittedBox(
+            child: FloatingActionButton(
+              backgroundColor: Color(0xFF46B5A6),
+              onPressed: () => scan(context),
+              child: Container(
+                child: Image.asset(
+                  'assets/barcode.png',
+                  width: ScreenUtil.instance.setWidth(30),
+                ),
+              ),
+              materialTapTargetSize: MaterialTapTargetSize.padded,
+            ),
+          ),
+        ),
+      ),
       // bottomNavigationBar: Container(
       //     margin: EdgeInsets.only(
       //         left: ScreenUtil.instance.setWidth(20),
@@ -73,8 +118,6 @@ class AssetDetail extends StatelessWidget{
       //   )
     );
   }
-
- 
 }
 
 class AssetDetails extends StatefulWidget {
@@ -82,12 +125,9 @@ class AssetDetails extends StatefulWidget {
   State<StatefulWidget> createState() {
     return AssetDetailState();
   }
-
 }
 
 class AssetDetailState extends State<AssetDetails> {
-
-  
   TextEditingController codectrl = new TextEditingController();
   String codetext = '';
   bool code = !true;
@@ -103,15 +143,15 @@ class AssetDetailState extends State<AssetDetails> {
   TextEditingController prjctrl = new TextEditingController();
   String prjtext = '';
   bool prj = !true;
-  
+
   TextEditingController unamectrl = new TextEditingController();
   String unametext = '';
   bool uname = !true;
   TextEditingController remarkctrl = new TextEditingController();
   String remarktext = '';
   bool remark = !true;
-  
-  List<File> imglist = [];  
+
+  List<File> imglist = [];
   File img;
 
   Widget imgContainer() {
@@ -122,7 +162,8 @@ class AssetDetailState extends State<AssetDetails> {
         children: <Widget>[
           SizedBox(height: ScreenUtil.instance.setHeight(20)),
           Icon(Icons.photo_camera, size: ScreenUtil.instance.setSp(60)),
-          Text('NO IMAGE', style: TextStyle(fontSize: ScreenUtil.instance.setSp(40)))
+          Text('NO IMAGE',
+              style: TextStyle(fontSize: ScreenUtil.instance.setSp(40)))
         ],
       );
     }
@@ -131,56 +172,61 @@ class AssetDetailState extends State<AssetDetails> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).requestFocus(new FocusNode());
-      },
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: ScreenUtil.instance.setHeight(200),
-              child: imgContainer()
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              color: Colors.grey[300],
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.format_list_bulleted),
-                  Text(' Project :  Project Name')
-                ],
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Container(
+                  height: ScreenUtil.instance.setHeight(200),
+                  child: imgContainer()),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.grey[300],
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.format_list_bulleted),
+                    Text(' Project :  Project Name')
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: ListView(
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                children: <Widget>[
-                  textfield(Icons.insert_drive_file, ' Asset Code', '$codetext', codectrl, code),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.apps, " Asset Name", nametext, namectrl, name),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.menu, " Serial Number", serialtext, serialctrl, serial),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.format_align_left, " Project", prjtext, prjctrl, prj),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.format_align_left, " Quantity", qtytext, qtyctrl, qty),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.format_align_left, " Unit Name", unametext, unamectrl, uname),
-                  SizedBox(height: ScreenUtil.instance.setHeight(20)),
-                  textfield(Icons.bookmark_border, " Asset Remark", remarktext, remarkctrl, remark),
-                  
-                ],
+              Container(
+                padding: EdgeInsets.all(20),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  children: <Widget>[
+                    textfield(Icons.insert_drive_file, ' Asset Code',
+                        '$codetext', codectrl, code),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(
+                        Icons.apps, " Asset Name", nametext, namectrl, name),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(Icons.menu, " Serial Number", serialtext,
+                        serialctrl, serial),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(Icons.format_align_left, " Project", prjtext,
+                        prjctrl, prj),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(Icons.format_align_left, " Quantity", qtytext,
+                        qtyctrl, qty),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(Icons.format_align_left, " Unit Name", unametext,
+                        unamectrl, uname),
+                    SizedBox(height: ScreenUtil.instance.setHeight(20)),
+                    textfield(Icons.bookmark_border, " Asset Remark",
+                        remarktext, remarkctrl, remark),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ));
+            ],
+          ),
+        ));
   }
 
-   Widget textfield(icon, text, hinttext, controller, enabled) {
+  Widget textfield(icon, text, hinttext, controller, enabled) {
     if (hinttext == '') {
       return Column(
         children: <Widget>[
@@ -191,7 +237,9 @@ class AssetDetailState extends State<AssetDetails> {
               Text('$text'),
             ],
           ),
-          SizedBox(height: ScreenUtil.instance.setHeight(5),),
+          SizedBox(
+            height: ScreenUtil.instance.setHeight(5),
+          ),
           TextFormField(
             controller: controller,
             enabled: enabled,
@@ -205,7 +253,6 @@ class AssetDetailState extends State<AssetDetails> {
               // ),
             ),
           )
-
         ],
       );
     } else {
@@ -218,7 +265,9 @@ class AssetDetailState extends State<AssetDetails> {
               Text('$text'),
             ],
           ),
-          SizedBox(height: ScreenUtil.instance.setHeight(5),),
+          SizedBox(
+            height: ScreenUtil.instance.setHeight(5),
+          ),
           TextFormField(
             controller: controller,
             enabled: enabled,
@@ -232,11 +281,8 @@ class AssetDetailState extends State<AssetDetails> {
               // ),
             ),
           )
-
         ],
       );
-
     }
   }
-
 }
